83feaf8456219fd20b1e5b8f93fb9a25cc989d58
Lachi <brother1020big@gmail.com>
2022-04-12 23:45:27 +0900

    Sort Algorithm, but error

---
+++ Algorithms/heapSort.cpp
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include<iostream>
+
+#define N 1000
+using namespace std;
+
+void HeapSort(int A[], int n);
+void Buildheap(int A[], int n);
+void heapify(int A[], int k, int n);
+
+
+
+void HeapSort(int A[], int n)
+{
+    int t;
+    Buildheap(A, n);   
+    for (int i = n; i >= 1; i--)        
+    {
+        
+        t = A[0];
+        A[0] = A[i];
+        A[i] = t;
+
+        
+        heapify(A, 0, i - 1);
+    }
+}
+
+void Buildheap(int A[], int n)
+{
+    for (int i = n / 2; i >= 0; i--)    
+    {
+        heapify(A, i, n);   
+    }
+}
+
+
+void heapify(int A[], int k, int n)
+{
+    int left = k * 2;
+    int right = k * 2 + 1;         
+    int smaller;                   
+    int t;
+
+    if (right <= n)
+    {
+        if (A[left] < A[right])
+            smaller = left;
+        else smaller = right;
+    }
+    else if (left <= n) 
+    {
+        smaller = left;
+    }
+    else return;       
+
+   
+    if (A[smaller] < A[k])
+    {
+        t = A[k];
+        A[k] = A[smaller];
+        A[smaller] = t;
+        heapify(A, smaller, n);
+    }
+}
+
+void print(int* DataSet, int Length, const char* str)
+{
+    cout << str << '\n';
+    for (int i = 0; i < Length; i++) cout << DataSet[i] << " ";
+    cout << "\n\n";
+}
+
+int main(void)
+{
+    int n, DataSet[N];
+
+    cout << "insert n" << endl;
+    cin >> n;
+    int Length = n;
+    int i = 0;
+   
+    srand((unsigned int)time(NULL));
+    for (i = 0; i < Length-1; i++)
+    {
+        for (int j = i+1; j < Length; j++) {
+            DataSet[i] = (int)(rand() / (double)RAND_MAX * 100);
+
+            if (DataSet[j] < DataSet[i]) {
+                int temp = DataSet[j];
+                DataSet[j] = DataSet[i];
+                DataSet[i] = temp;
+
+            }
+        }
+        
+    }
+    
+  
+    print(DataSet, Length, "[Èü Á¤·Ä ÀÌÀü]");
+    HeapSort(DataSet, n);   
+    print(DataSet, Length, "[Èü Á¤·Ä ÀÌÈÄ]");
+
+ 
+
+
+
+
+
+  
+   
+    return 0;
+}
---
+++ Algorithms/mergeSort.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <ctime>
+
+#define MAX_ARR 100000
+
+using namespace std;
+
+void merge(int* arr, int p, int mid, int r)
+{
+	int* sorted = new int[r - p + 1];
+	int i, j, k;
+	i = p;		
+	j = mid + 1;	
+	k = 0;			
+
+	while (i <= mid && j <= r)
+	{
+		if (arr[i] <= arr[j]) sorted[k++] = arr[i++];
+		else sorted[k++] = arr[j++];
+	}
+
+	if (i > mid)
+		while (j <= r) sorted[k++] = arr[j++];
+	else
+		while (i <= mid) sorted[k++] = arr[i++];
+
+	for (i = p, k = 0; i <= r; i++, k++) arr[i] = sorted[k];
+
+	delete[] sorted;
+}
+
+void mergeSort(int* arr, int p, int r)
+{
+	if (p < r)
+	{
+		int mid = (p + r) / 2;
+		mergeSort(arr, p, mid);
+		mergeSort(arr, mid + 1, r);
+		merge(arr, p, mid, r);
+	}
+}
+
+void print(int* arr, int N, const char* str)
+{
+	cout << str << '\n';
+	for (int i = 0; i < N; i++) cout << arr[i] << " ";
+	cout << "\n\n";
+}
+
+int main(void)
+{
+	int temp;
+	srand(time(NULL));
+
+
+
+
+	int n, arr[MAX_ARR];
+
+	cout << "insert n" << endl;
+	cin >> n;
+
+
+	for (int i = 0; i < n ; i++) {
+		arr[i] = (rand() % 10000) + 1;
+		for (int j = i; j < n; j++) {
+			
+			
+			if (arr[i] < arr[j]) {
+				
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	
+	print(arr, n, "[병합 정렬 이전]");
+	mergeSort(arr, 0, n - 1);
+	print(arr, n, "[병합 정렬 이후]");
+
+
+
+	return 0;
+}
